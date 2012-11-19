4fbecf34cecd82d2e59d0692197acc2af733a962
Mike <mike.d.meier@gmail.com>
2012-11-19 15:40:45 -0500

    update code to v2.1

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-import re
-
 ####################################################################################################
 
 VIDEO_PREFIX = "/video/rte"
@@ -17,6 +14,15 @@ MRSS  = {'media':'http://search.yahoo.com/mrss/'}
 RTE   = {'rte':'http://www.rte.ie/schemas/vod'}
 
 ####################################################################################################
+def Start():
+    ObjectContainer.art = R(ART)
+    ObjectContainer.title1 = NAME
+    DirectoryObject.thumb = R(ICON)
+    
+    Dict['geo_code']=str(Geo())
+
+####################################################################################################
+@route('/video/rte/geo')
 def Geo():
     country_code = HTML.ElementFromURL(FEEDBASE+'cl/').xpath("//geoinfo/country")[0]
     if country_code.text == "ie":
@@ -24,21 +30,8 @@ def Geo():
     else:
       return "#international"
 
-def Start():
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, NAME, ICON, ART)
-
-    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
-
-    MediaContainer.art = R(ART)
-    MediaContainer.title1 = NAME
-    DirectoryItem.thumb = R(ICON)
-    WebVideoItem.thumb = R(ICON)
-
-    HTTP.CacheTime = CACHE_1HOUR
-    
-    Dict['geo_code']=str(Geo())
-
+####################################################################################################
+@route('/video/rte/updatecache')
 def UpdateCache():
   HTTP.PreCache(FEEDBASE + "videos/live/?id=7")
   HTTP.PreCache(FEEDBASE + "latest/?limit=20")
@@ -52,60 +45,69 @@ def UpdateCache():
   HTTP.PreCache(FEEDBASE + "genre/?id=Religious%20and%20Irish%20Language")
   HTTP.PreCache(FEEDBASE + "genre/?id=RT%C3%89jr%2C%20TRT%C3%89%2C%20Two%20Tube")
 
-def VideoMainMenu():
-
-    dir = MediaContainer(viewGroup="List")
+####################################################################################################
+@handler('/video/rte', NAME, thumb=icon, art=ART)
+def MainMenu():
+    oc = ObjectContainer()
+    
     #Live Stream
-    feed = (HTTP.Request(FEEDBASE + "videos/live/?id=7").content).encode("Latin1",'ignore').replace('media:','media')
-    for entry in HTML.ElementFromString(feed).xpath("//feed/entry"):
-      desc = entry.xpath("content")[0].text
-      thumb = entry.xpath("mediathumbnail")[0].get('url')
-      link = LIVEURL + Dict['geo_code']
+    if Platform.HasFlash:
+        feed = RSS.FeedFromURL(FEEDBASE + "videos/live/?id=7")
+        desc = feed.items[0].description
+        thumb = feed.items[0].media_thumbnail['url']
+        link = LIVEURL + Dict['geo_code']
+        oc.add(VideoClipObject(url=link, title="Live", summary=desc, thumb=thumb))
 
-      dir.Append(WebVideoItem(url=link,title="Live",summary=desc,thumb=thumb))
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"latest/?limit=20", title="Latest"), title="Latest"))
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"lastchance/?limit=20", title="Last Chance"), title="Last Chance"))
+    oc.add(DirectoryObject(key=Callback(CategoriesSubMenu), title="Categories"))
+    oc.add(DirectoryObject(key=Callback(AZSubMenu), title="A to Z"))
+    
+    return oc
 
-    dir.Append(Function(DirectoryItem(RSS_parser,"Latest"),pageurl = FEEDBASE + "latest/?limit=20"))
-    dir.Append(Function(DirectoryItem(RSS_parser,"Last Chance"),pageurl = FEEDBASE + "lastchance/?limit=20"))
-    dir.Append(Function(DirectoryItem(CategoriesSubMenu,"Categories")))
-    dir.Append(Function(DirectoryItem(AZSubMenu,"A to Z")))
-    return dir
+####################################################################################################
+@route('/video/rte/categories')
+def CategoriesSubMenu():
+    oc = ObjectContainer(title2="Categories")
 
-def CategoriesSubMenu (sender):
-    dir = MediaContainer(title2="Categories", viewGroup="List")
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"genre/?id=Entertainment", title="Entertainment"), title="Entertainment"))
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"genre/?id=Drama", title="Drama"), title="Drama"))
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"genre/?id=News%20and%20Sport", title="News and Sporst"), title="News and Sports"))
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"genre/?id=Factual", title="Factual"), title="Factual"))
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"genre/?id=Lifestyle", title="Lifestyle"), title="Lifestyle"))
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"genre/?id=Arts%20and%20Music", title="Arts and Music"), title="Arts and Music"))
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"genre/?id=Religious%20and%20Irish%20Language", title="Religious and Irish language"), title="Religious and Irish language"))
+    oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+"genre/?id=RT%C3%89jr%2C%20TRT%C3%89%2C%20Two%20Tube", title="RTÉjr, TRTÉ, Two Tube"), title="RTÉjr, TRTÉ, Two Tube"))
 
-    dir.Append(Function(DirectoryItem(RSS_parser,"Entertainment"),pageurl = FEEDBASE + "genre/?id=Entertainment"))
-    dir.Append(Function(DirectoryItem(RSS_parser,"Drama"),pageurl = FEEDBASE + "genre/?id=Drama"))
-    dir.Append(Function(DirectoryItem(RSS_parser,"News and Sports"),pageurl = FEEDBASE + "genre/?id=News%20and%20Sport"))
-    dir.Append(Function(DirectoryItem(RSS_parser,"Factual"),pageurl = FEEDBASE + "genre/?id=Factual"))
-    dir.Append(Function(DirectoryItem(RSS_parser,"Lifestyle"),pageurl = FEEDBASE + "genre/?id=Lifestyle"))
-    dir.Append(Function(DirectoryItem(RSS_parser,"Arts and Music"),pageurl = FEEDBASE + "genre/?id=Arts%20and%20Music"))
-    dir.Append(Function(DirectoryItem(RSS_parser,"Religious and Irish language"),pageurl = FEEDBASE + "genre/?id=Religious%20and%20Irish%20Language"))
-    dir.Append(Function(DirectoryItem(RSS_parser,"RTÉjr, TRTÉ, Two Tube"),pageurl = FEEDBASE + "genre/?id=RT%C3%89jr%2C%20TRT%C3%89%2C%20Two%20Tube"))
+    return oc
 
-    return dir
-
-def AZSubMenu (sender):
-    dir = MediaContainer(title2="A to Z", viewGroup="List")
+####################################################################################################
+@route('/video/rte/atoz')
+def AZSubMenu():
+    oc = ObjectContainer(title2="A to Z")
 
     for entry in HTML.ElementFromURL(FEEDBASE+'azlist/').xpath("//entry"):
-      if entry.xpath("title")[0].text == None:
-        continue
-      else:
-        character = entry.xpath("title")[0].text
-      dir.Append(Function(DirectoryItem(RSS_parser,character),pageurl = FEEDBASE + "az/?id="+character))
-    return dir
+        if entry.xpath("title")[0].text == None:
+            continue
+        else:
+            character = entry.xpath("title")[0].text
+        oc.add(DirectoryObject(key=Callback(RSS_parser, pageurl=FEEDBASE+'az/?id='+character, title=character), title=character))
 
-def RSS_parser(sender, pageurl , replaceParent=False):
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList", replaceParent=replaceParent)
+    return oc
 
-    feed = (HTTP.Request(pageurl).content).encode("Latin1","ignore").replace('media:','media').replace('rte:','rte')
-    for entry in HTML.ElementFromString(feed).xpath("//entry"):
-      title = entry.xpath("title")[0].text
-      desc = entry.xpath("content")[0].text
-      duration = entry.xpath("rteduration")[0].get('ms')
-      thumb = entry.xpath("mediathumbnail")[0].get('url')
-      link = entry.xpath("link[@rel='alternate']")[0].get('href') + Dict['geo_code']
+####################################################################################################
+@route('/video/rte/rss')
+def RSS_parser(pageurl, title):
+    oc = MediaContainer(title2=title)
 
-      dir.Append(WebVideoItem(url=link,title=title,summary=desc,thumb=thumb,duration=duration))
+    feed = RSS.FeedFromURL(url)
+    i = 0
+    while i < len(feed.items):
+        title = feed.items[i].title
+        desc = feed.items[i].description
+        duration = feed.items[i].rte_duration['ms']
+        thumb = feed.items[i].media_thumbnail['url']
+        link = feed.items[i].link
+        oc.add(VideoClipObject(url=link, title=title, summary=desc, duration=duration, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
 
-    return dir
+    return oc
